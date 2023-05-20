b6074358c2d93b111296f4f0936defcc6fa0ceba
TurtlePower <agnkvdm@gmail.com>
2023-05-20 13:53:27 +0300

    add mobile menu js

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,41 @@
+const refs = {
+  mobileMenuOpenBtn: document.querySelector('.js-open-menu'),
+  mobileMenuCloseBtn: document.querySelector('.js-close-menu'),
+  backdrop: document.querySelector('.js-backdrop'),
+  mobileMenuContainer: document.querySelector('.js-mobile-menu'),
+};
+
+refs.mobileMenuOpenBtn.addEventListener('click', toggleMobileMenu);
+refs.mobileMenuCloseBtn.addEventListener('click', toggleMobileMenu);
+
+// Close the mobile menu on wider screens if the device orientation changes
+window
+  .matchMedia('(min-width: 768px)')
+  .addEventListener('change', onWindowSizeChange);
+
+function toggleMobileMenu() {
+  refs.backdrop.classList.toggle('is-hidden');
+  refs.mobileMenuContainer.classList.toggle('is-closed');
+
+  if (
+    !refs.backdrop.classList.contains('is-hidden') &&
+    !refs.mobileMenuContainer.classList.contains('is-closed')
+  ) {
+    refs.backdrop.addEventListener('click', onBackdropClick);
+  } else {
+    refs.backdrop.removeEventListener('click', onBackdropClick);
+  }
+}
+
+function onWindowSizeChange(e) {
+  if (!e.matches) return;
+  refs.backdrop.classList.add('is-hidden');
+  refs.mobileMenuContainer.classList.add('is-closed');
+}
+
+function onBackdropClick(e) {
+  const isBackdrop = e.target.classList.contains('backdrop');
+  if (isBackdrop) {
+    toggleMobileMenu();
+  }
+}
